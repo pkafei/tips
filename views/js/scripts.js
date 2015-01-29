b77b2973c3a56269e08bfd102767613685213d7a
Portia Burton <plburton@gmail.com>
2015-01-29 12:59:55 -0800

    First attempt to add fields meals and tips

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,15 +1,26 @@
-//Ask user to enter total tab
-//Ask user to enter total tip
-//Store and add tip and total in weekily section
-//Stor and add tip in monthly section
+//Add meal amount and tip
+//Store and add tip and total in weekly section
+//Store and add tip in monthly section
+//Store and add tip in yearly section
+//Have values update dynamically
 
-var main = function() {
-	"use strict";
+//var theForm = document.forms["meal_tip"];
 
-	$(".meal-input button").on("click", function(event){
-		console.log("How much did you spend?");
-	});
+function mealCost() {
+	//
+	 //Declare variable meal and tips
+	 var meal_cost = document.getElementsbyName('number');
+	 var total_cost = 0;
 
-	$(document).ready(main);
-
+	 // Add meal and tips
+	 for(var i=0, i<meal_cost.length; i++) {
+	 		total_cost += meal_cost[i].value;
+	 }
+	 // Log variable meals and tips
+	 document.getElementById('week').value = total_cost;
+	 // Return variable meals and tips
+	 
 }
+
+//Notes
+//http://stackoverflow.com/questions/5338716/get-multiple-elements-by-id
