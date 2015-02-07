f436025c6bd2bc1f3babae46dc57e4c36100234f
Portia Burton <plburton@gmail.com>
2015-02-06 22:58:38 -0800

    Mostly comments

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,26 +1,14 @@
 //Add meal amount and tip
-//Store and add tip and total in weekly section
-//Store and add tip in monthly section
-//Store and add tip in yearly section
+//Store and add meal + tip and total in weekly section
+//Store and add meal + tip in monthly section
+//Store and add meal + tip in yearly section
 //Have values update dynamically
 
-//var theForm = document.forms["meal_tip"];
+	//Validate Cost of Meal and Tip
 
-function mealCost() {
-	//
-	 //Declare variable meal and tips
-	 var meal_cost = document.getElementsbyName('number');
-	 var total_cost = 0;
 
-	 // Add meal and tips
-	 for(var i=0, i<meal_cost.length; i++) {
-	 		total_cost += meal_cost[i].value;
-	 }
-	 // Log variable meals and tips
-	 document.getElementById('week').value = total_cost;
-	 // Return variable meals and tips
-	 
-}
 
 //Notes
 //http://stackoverflow.com/questions/5338716/get-multiple-elements-by-id
+//Might need some knowledge of jquery
+//http://stackoverflow.com/questions/18226598/how-to-add-a-button-dynamically-using-jquery
